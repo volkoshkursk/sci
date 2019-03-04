916fd9bd4e24df9d07c441f9f869a2ffa1e1b8e7
volkoshkursk <zaz-pagani@inbox.ru>
2019-03-04 20:29:56 +0300

    add auto adaptation

--- adaptation.py
+++ adaptation.py
@@ -16,5 +16,9 @@ for i in range(5):
         params[i] += params[i] * 0.1
         micro, macro = new_micro, new_macro
         new_micro, new_macro = main_lda(params[0], params[1], params[2], params[3], params[4])
+    while mid(new_micro, new_macro) <= mid(micro, macro):
+        params[i] -= params[i] * 0.1
+        micro, macro = new_micro, new_macro
+        new_micro, new_macro = main_lda(params[0], params[1], params[2], params[3], params[4])
     print(i, end=' ')
     print(params[i])
